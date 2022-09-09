d0098810b1e2a2ff5ead67f50e91bb08f1b9a03e
Ashanks70 <ashanks70@charlottesvilleschools.org>
2022-09-09 10:43:27 -0400

    hi

--- raspberry-pi/launchpad.py
+++ raspberry-pi/launchpad.py
@@ -2,19 +2,23 @@
 import board
 import time
 import digitalio
-g=digitalio.DigitalInOut(board.GP16)
-r=digitalio.DigitalInOut(board.GP15)
+g=digitalio.DigitalInOut(board.GP16)#green LED
+r=digitalio.DigitalInOut(board.GP15)#Red LED
+button=digitalio.DigitalInOut(board.GP3)#button
+button.direction = digitalio.Direction.INPUT
 r.direction = digitalio.Direction.OUTPUT
 g.direction = digitalio.Direction.OUTPUT
 x=10
-print(x)
-time.sleep(1)
-for i in range(10):
-    if x>0:
-        x-=1
+while True:
+    if button.value==False:
+        break
+for i in range(10):#run 10 times
+    if x>0:#run through all numbers
         print(x)
-
+        x-=1
         if x==0:
+            time.sleep(1)
+            print(x)
             r.value=True
             time.sleep(1)
             r.value=False
