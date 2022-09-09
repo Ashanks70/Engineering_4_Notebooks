e80a354228997b47df51db4f4c4d2d9482011ba8
Ashanks70 <ashanks70@charlottesvilleschools.org>
2022-09-09 10:16:27 -0400

    hi

--- raspberry-pi/launchpad.py
+++ raspberry-pi/launchpad.py
@@ -4,6 +4,8 @@ import time
 import digitalio
 g=digitalio.DigitalInOut(board.GP16)
 r=digitalio.DigitalInOut(board.GP15)
+r.direction = digitalio.Direction.OUTPUT
+g.direction = digitalio.Direction.OUTPUT
 x=10
 print(x)
 time.sleep(1)
@@ -11,4 +13,13 @@ for i in range(10):
     if x>0:
         x-=1
         print(x)
-        time.sleep(1)
+
+        if x==0:
+            r.value=True
+            time.sleep(1)
+            r.value=False
+        else:
+            g.value=True
+            time.sleep(.5)
+            g.value=False
+            time.sleep(.5)
