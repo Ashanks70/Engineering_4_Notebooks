3caa93389533ea4b179c3a75199b8b7960a238ca
Ashanks70 <ashanks70@charlottesvilleschools.org>
2022-09-21 09:54:54 -0400

    hi

--- raspberry-pi/Accelerometer.py
+++ raspberry-pi/Accelerometer.py
@@ -7,13 +7,18 @@ import digitalio
 import terminalio
 import displayio
 import busio
+import adafruit_mpl3115a2
 import adafruit_mpu6050
 displayio.release_displays()
 led=digitalio.DigitalInOut(board.GP12)
 led.direction=digitalio.Direction.OUTPUT
 sda_pin = board.GP10#other is 0x3d
 scl_pin = board.GP15
+scl2 = board.GP13
+sda2 = board.GP8
 i2c = busio.I2C(scl_pin, sda_pin)
+i3c = busio.I2C(scl2,sda2)
+sensor = adafruit_mpl3115a2.MPL3115A2(i3c)
 mpu = adafruit_mpu6050.MPU6050(i2c, 0x68)#0x68
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP6)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
@@ -22,11 +27,12 @@ title = "ANGULAR VELOCITY"
 xvalue = "X rotation = "
 yvalue = "Y rotation = "
 zvalue = "Z rotation = "
-
-
+sensor.sealevel_pressure = 103040
+h=sensor.altitude
 while True:
     print("Acceleration: ","x =" , round(mpu.acceleration[0],3), "," ,"y =",round(mpu.acceleration[1],3),",","z = ",round(mpu.acceleration[2],3))
-    if abs(mpu.acceleration[0])>=8.8  or abs(mpu.acceleration[1])>=8.8:
+    print(sensor.altitude-h)
+    if sensor.altitude-h<3 and (abs(mpu.acceleration[0])>=8.8  or abs(mpu.acceleration[1])>=8.8):
         led.value=True
     else:
         led.value=False
